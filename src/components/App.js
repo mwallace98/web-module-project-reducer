d642575f36518ca77f22e4bf05336d16cedfef95
mwallace98 <mattwallace140@gmail.com>
2023-10-26 21:27:45 -0400

    1 Button adds 1

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,10 @@ import CalcButton from './CalcButton';
 function App() {
 
   const [state,dispatch] = useReducer(reducer,initialState)
+
+  const handle1Click = () => {
+    dispatch(addOne())
+  }
   
   console.log(state)
   return (
@@ -36,7 +40,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton value={1} onClick ={handle1Click} />
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
